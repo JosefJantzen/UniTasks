5d0dbf260224a36eff62b129ddee6d40a337e25d
JosefJantzen <jochef2.dev@gmail.com>
2022-12-01 18:41:53 +0100

    recurringTask model

---
+++ backend/models/recurringTask.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/google/uuid"
+
+type RecurringTask struct {
+	id          uuid.UUID
+	name        string
+	description string
+	interval    string
+	parentUser  uuid.UUID
+}

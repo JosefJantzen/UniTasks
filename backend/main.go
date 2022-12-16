f1582713a8353ea5194e1ef7331f45c29860ce0b
Josef Jantzen <jochef2.dev@gmail.com>
2022-12-16 15:03:00 +0100

    add config support

--- backend/main.go
+++ backend/main.go
@@ -6,12 +6,18 @@ import (
 	_ "github.com/cockroachdb/cockroach-go/v2/crdb"
 
 	"unitasks.josefjantzen.de/backend/api"
+	"unitasks.josefjantzen.de/backend/config"
 	"unitasks.josefjantzen.de/backend/database"
 )
 
 func main() {
-	dbService := database.InitDB()
-	api.InitServer(dbService)
+	config, err := config.Read("config.sample.json")
+	if err != nil {
+		fmt.Println("Config read error: ", err)
+		return
+	}
+	dbService := database.InitDB(config)
+	api.InitServer(dbService, config)
 	fmt.Println("Completly Started")
 	select {}
 }

6648f75b8bc9f9bdff88c195f7726499900bc126
Josef Jantzen <jochef2.dev@gmail.com>
2022-12-18 23:02:26 +0100

    config backfall to sample if missing attribute

--- backend/config/config.go
+++ backend/config/config.go
@@ -12,6 +12,16 @@ type Config struct {
 	JwtExpireMin int
 }
 
+func (c *Config) merge(s *Config) {
+	c.DB.merge(s.DB)
+	if s.Port != "" {
+		c.Port = s.Port
+	}
+	if s.JwtExpireMin > c.JwtExpireMin {
+		c.JwtExpireMin = s.JwtExpireMin
+	}
+}
+
 type DB struct {
 	User     string
 	Pwd      string
@@ -19,6 +29,27 @@ type DB struct {
 	Port     string
 	Database string
 	Initial  string
+}
+
+func (c *DB) merge(s DB) {
+	if s.User != "" {
+		c.User = s.User
+	}
+	if s.Pwd != "" {
+		c.Pwd = s.Pwd
+	}
+	if s.Host != "" {
+		c.Host = s.Host
+	}
+	if s.Port != "" {
+		c.Port = s.Port
+	}
+	if s.Database != "" {
+		c.Database = s.Database
+	}
+	if s.Initial != "" {
+		c.Initial = s.Initial
+	}
 }
 
 func (db *DB) GetDBConnectString() string {
@@ -32,7 +63,17 @@ func (db *DB) GetDBConnectString() string {
 }
 
 func Read(file string) (*Config, error) {
-	buf, err := ioutil.ReadFile(file)
+	buf, err := ioutil.ReadFile("config.sample.json")
+	if err != nil {
+		return nil, err
+	}
+
+	var sampleConfig Config
+	if err := json.Unmarshal(buf, &sampleConfig); err != nil {
+		return nil, err
+	}
+
+	buf, err = ioutil.ReadFile(file)
 	if err != nil {
 		return nil, err
 	}
@@ -42,5 +83,7 @@ func Read(file string) (*Config, error) {
 		return nil, err
 	}
 
-	return &config, err
+	sampleConfig.merge(&config)
+
+	return &sampleConfig, err
 }

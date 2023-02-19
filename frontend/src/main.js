e0c71399f0b45f152f0c4aafaefbfef32ff23003
Josef Jantzen <jochef2.dev@gmail.com>
2023-02-19 19:46:55 +0100

    vuestic-ui initial

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
-createApp(App).mount('#app')
+createApp(App).use(createVuestic()).mount('#app')

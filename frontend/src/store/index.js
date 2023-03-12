1d5c3aa78e7c082c3accbccc0db6e4b93d9304c0
Josef Jantzen <jochef2.dev@gmail.com>
2023-03-12 17:44:45 +0100

    Added vuex-persist for persitent storage

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,17 @@
 import {createStore} from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 import user from './modules/user'
+
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+  })
 
 const store = createStore({
     modules: {
         user: user
-    }
+    },
+    plugins: [vuexLocal.plugin]
 })
 
 export default store

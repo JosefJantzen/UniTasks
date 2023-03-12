20d89c11a9aa18f0e13c6b0ab6384ba2499cdebe
Josef Jantzen <jochef2.dev@gmail.com>
2023-03-12 18:45:05 +0100

    Switching account avatars

--- frontend/src/components/navbar/AccountMenu.vue
+++ frontend/src/components/navbar/AccountMenu.vue
@@ -1,22 +1,32 @@
 <template>
     <va-navbar-item>
-        <va-dropdown>
+        <va-dropdown v-if="initial() != null">
             <template #anchor>
-                <va-button @click="foo">
-                    <va-avatar >{{ initial() }}</va-avatar>
-                </va-button>
-                
+                <va-button>
+                    <va-avatar size="small" color="secondary" style="font-size: 100%;">{{ initial() }}</va-avatar>
+                </va-button> 
             </template>
             <va-dropdown-content>
                 <va-button >Settings</va-button> <br>
                 <va-button >Logout</va-button>
             </va-dropdown-content>
-        </va-dropdown>   
+        </va-dropdown>  
+        <va-dropdown v-else>
+            <template #anchor>
+                <va-button>
+                    <va-avatar size="small" color="secondary">
+                        <va-icon name="mdi-account_circle"></va-icon>
+                    </va-avatar>
+                </va-button> 
+            </template>
+            <va-dropdown-content>
+                <va-button >Login</va-button> <br>
+            </va-dropdown-content>
+        </va-dropdown>  
     </va-navbar-item>
 </template>
 
 <script>
-
 export default {
     name: 'AccountMenu',
     methods: {

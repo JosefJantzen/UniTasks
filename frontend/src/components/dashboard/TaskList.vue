d014ab5ce1f1c7922103fa1f9bae59fcd1dc013d
Josef Jantzen <jochef2.dev@gmail.com>
2023-03-15 20:15:48 +0100

    task list items hover effect

--- frontend/src/components/dashboard/TaskList.vue
+++ frontend/src/components/dashboard/TaskList.vue
@@ -1,64 +1,27 @@
 <template>
-    <!--<va-list>
-        <va-list-item
-            v-for="(task, index) in this.$store.getters['getAllTasks']"
-            :key="index"
-            class="list__item"
-        >   
-            <va-list-item-section avatar>
-                <va-avatar v-if="task.recurring" icon="mdi-repeat" />
-                <va-avatar v-else icon="mdi-repeat_one" />
-            </va-list-item-section>
-            <va-list-item-section>
-                <va-list-item-label>
-                    {{ task.name }} 
-                </va-list-item-label>
-                <va-list-item-label caption>
-                    {{ task.desc }}
-                </va-list-item-label>
-            </va-list-item-section>
-            <va-list-item-section>
-                <div>
-                    <va-icon name="schedule"/> {{ getDue(task)}}
-                </div>
-            </va-list-item-section>
-            <va-list-separator></va-list-separator>
-            <va-list-item-section avatar>
-                <va-button icon="check" round class="btn"/>
-            </va-list-item-section>
-            <va-list-item-section avatar>
-                <va-button-dropdown color="light" icon="more_vert" opened-icon="more_vert" round placement="right-start">
+    <va-card 
+        v-for="(task, index) in this.$store.getters['getAllTasks']"
+        :key="index"
+    >
+        <div class="listItem hh">
+            <va-card-title>
+                <va-avatar v-if="task.recurring" icon="mdi-repeat" size="40px"/>
+                <va-avatar v-else icon="mdi-repeat_one" size="40px"/>
+                <h1 style="font-size: 20px; margin-left: 0.5rem;">{{ task.name }}</h1>
+                <va-button icon="mdi-check" round class="btn" style="margin-left: auto;"/>
+                <va-button-dropdown style="margin-left: 0.5rem;" preset="plain" icon="more_vert" opened-icon="more_vert" round placement="right-start">
                     <va-button class="drop-btn" preset="secondary" icon="visibility">&nbsp;&nbsp;Show</va-button>
                     <br>
                     <va-button class="drop-btn" preset="secondary" icon="edit">&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;</va-button>
                     <br>
                     <va-button class="drop-btn" preset="secondary" icon="delete">Delete</va-button>
                 </va-button-dropdown>
-            </va-list-item-section>
-        </va-list-item>
-    </va-list>-->
-    <va-card 
-        v-for="(task, index) in this.$store.getters['getAllTasks']"
-        :key="index"
-        class="list__item"
-    >
-        <va-card-title>
-            <va-avatar v-if="task.recurring" icon="mdi-repeat" size="40px"/>
-            <va-avatar v-else icon="mdi-repeat_one" size="40px"/>
-            <h1 style="font-size: 20px; margin-left: 0.5rem;">{{ task.name }}</h1>
-            <va-button icon="mdi-check" round class="btn" style="margin-left: auto;"/>
-            <va-button-dropdown style="margin-left: 0.5rem;" preset="plain" icon="more_vert" opened-icon="more_vert" round placement="right-start">
-                <va-button class="drop-btn" preset="secondary" icon="visibility">&nbsp;&nbsp;Show</va-button>
-                <br>
-                <va-button class="drop-btn" preset="secondary" icon="edit">&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;</va-button>
-                <br>
-                <va-button class="drop-btn" preset="secondary" icon="delete">Delete</va-button>
-            </va-button-dropdown>
-        </va-card-title>
-        <va-card-content style="display: flex;">
-            <va-icon name="schedule"/> 
-            <p style="margin-top: auto; margin-bottom: auto; margin-left: 0.5rem;"> {{ getDue(task)}} </p>
-        </va-card-content>
+            </va-card-title>
+            <va-card-content style="display: flex;">
+                <va-icon name="schedule"/> 
+                <p style="margin-top: auto; margin-bottom: auto; margin-left: 0.5rem;"> {{ getDue(task)}} </p>
+            </va-card-content>
+        </div>
     </va-card>
 </template>
 
@@ -81,6 +44,11 @@ export default {
     created () {
         this.list()
         this.listRecurring()
+    },
+    data () {
+        return {
+            hoverItem: false
+        }
     }
 }
 
@@ -88,8 +56,13 @@ export default {
 
 <style>
 
-.list__item + .list__item {
+.listItem{
   margin-top: 20px;
+  cursor: pointer;
+}
+
+.hh:hover {
+    background-color: #d5e8e8;
 }
 
 .card-content {

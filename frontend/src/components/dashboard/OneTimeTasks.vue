ad993b29787d57593451fd9ddc80b3f3d20ec7dc
Josef Jantzen <jochef2.dev@gmail.com>
2023-03-17 16:02:45 +0100

    OneTime tasks layout

--- frontend/src/components/dashboard/OneTimeTasks.vue
+++ frontend/src/components/dashboard/OneTimeTasks.vue
@@ -1,11 +1,44 @@
 <template>
-    OneTimeTasks
+    <va-card 
+        v-for="(task, index) in this.$store.getters['tasks/getAll']"
+        :key="index"
+    >
+        <div class="listItem">
+            <va-card-title>
+                <h1 style="font-size: 20px;">{{ task.name }}</h1>
+                <div style="margin-left: auto; display: inline-block;">
+                    <va-icon name="schedule" /> 
+                    <span style="margin-top: auto; margin-bottom: auto; margin-left: 0.5rem; font-size: small;"> {{ getDue(task)}} </span>
+                </div>
+                <va-button icon="mdi-check" round class="btn" style="margin-left: auto;"/>
+                <va-button-dropdown style="margin-left: 0.5rem;" preset="plain" icon="more_vert" opened-icon="more_vert" round placement="right-start">
+                    <va-button class="drop-btn" preset="secondary" icon="visibility">&nbsp;&nbsp;Show</va-button>
+                    <br>
+                    <va-button class="drop-btn" preset="secondary" icon="edit">&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;</va-button>
+                    <br>
+                    <va-button class="drop-btn" preset="secondary" icon="delete">Delete</va-button>
+                </va-button-dropdown>
+            </va-card-title>
+        </div>
+    </va-card>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import help from '../../help/help'
 
 export default {
-    name: 'OneTimeTasks'
+    name: 'OneTimeTasks',
+    methods: {
+        ...mapActions('tasks', ['list']),
+        ...mapActions('tasks', ['getAll']),
+        getDue (task) {
+            if (task.recurring) {
+                    return  help.getDueString(task.ending.substring(0,10))
+                }
+                return help.getDueString(task.due.substring(0,10))
+        }
+    }
 }
 
 </script>

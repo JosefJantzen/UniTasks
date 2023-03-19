dd86178d18ad5930e8963fc65da66eefef2a8b98
Josef Jantzen <jochef2.dev@gmail.com>
2023-03-19 18:30:20 +0100

    task recurring history layout

--- frontend/src/components/TaskView.vue
+++ frontend/src/components/TaskView.vue
@@ -1,22 +1,28 @@
 <template>
     <div class="" style="display: flex;">
-            <va-button icon="mdi-close" size="small" preset="secondary" @click="this.$emit('click')"/>
+            <va-button icon="mdi-close" size="small" round preset="secondary" @click="this.$emit('click')"/>
             <h1 style="margin: auto 1rem; font-size: 25px;">{{ task.name }}</h1>
             <va-button icon="mdi-check" size="small" round v-if="!task.done" style="margin-left: auto;"/>
-            <va-button icon="mdi-edit" size="small" preset="secondary" style="margin-left: 0.5rem;"/>
-            <va-button icon="mdi-delete" size="small" preset="secondary" style="margin-left: 0.5rem;" />
+            <va-button icon="mdi-edit" size="small" round preset="secondary" :style="task.done ? 'margin-left: auto;' : 'margin-left: 0.5rem;'"/>
+            <va-button icon="mdi-delete" size="small" round preset="secondary" style="margin-left: 0.5rem;" />
     </div>
     <br>
     <div style="margin-left: 1em; margin-right: 1em;">
         <p style="text-decoration: #99a4a5;">{{ task.desc }}</p><br>
-        <div style="display: flex;">
-            <va-icon name="mdi-schedule"/>
-            <span style="margin-left: 1rem; margin-top: auto; margin-bottom: auto;">{{ this.getDueString() }}</span>
+        <div>
+            <div style="display: flex;">
+                <div style="display: flex; margin: auto 0;">
+                    <va-icon name="mdi-schedule"/>
+                    <span style="margin-left: 1rem; margin-top: auto; margin-bottom: auto;">{{ this.getDueString() }}</span>
+                </div>
+                <va-avatar v-if="task.recurring" size="40px" font-size="15px" style="margin-left: auto;">{{ task.count }}/{{ task.countMax }}</va-avatar>
+            </div>
+            <div style="display: flex; margin-top: 1rem;">
+                <va-icon v-if="task.done" name="mdi-check_circle"/>
+                <span v-if="task.done" style="margin-left: 1rem; margin-top: auto; margin-bottom: auto;">{{ this.getDoneAtString() }}</span>
+            </div>
         </div>
-        <div v-if="task.done" style="display: flex; margin-top: 1rem;">
-            <va-icon name="mdi-check_circle"/>
-            <span style="margin-left: 1rem; margin-top: auto; margin-bottom: auto;">{{ this.getDoneAtString() }}</span>
-        </div>
+        
     </div>
 </template>
 
